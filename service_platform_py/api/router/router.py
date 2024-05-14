2f6565c0e94ac0d0f4536980725cba9350f89f79
roxrook <atbl1511@gmail.com>
2024-05-14 13:32:42 -0700

    Remove user-client router.

--- service_platform_py/api/router/router.py
+++ service_platform_py/api/router/router.py
@@ -2,14 +2,8 @@ from fastapi.routing import APIRouter
 
 from service_platform_py.api.router.health import health_router
 from service_platform_py.api.router.user import user_router
-from service_platform_py.api.router.user import user_client_router
 
 api_router = APIRouter()
 # register all routers
 api_router.include_router(health_router, prefix="/health", tags=["health"])
 api_router.include_router(user_router, prefix="/user", tags=["user"])
-api_router.include_router(
-    user_client_router,
-    prefix="/user-client",
-    tags=["user-client"],
-)
--- service_platform_py/api/router/user/__init__.py
+++ service_platform_py/api/router/user/__init__.py
@@ -1,3 +1,2 @@
-__all__ = ["user_router", "user_client_router"]
+__all__ = ["user_router"]
 from .route import router as user_router
-from .route_client import router as user_client_router
